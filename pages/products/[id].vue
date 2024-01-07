7c2dbebe8bdae530376f56cb77325358fb31c907
Pontus Frendin <pontus@frend.in>
2024-01-07 20:06:42 +0000

    Fetch data and components

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <p>Product details for {{ id }}</p>
-        <p>Lorem ipsum</p>
+        <ProductDetails :product="product" />
     </div>
 </template>
 
 <script setup>
     const { id } = useRoute().params
+    const uri = "https://fakestoreapi.com/products/" + id
+
+    const { data: product } = await useFetch(uri, { key: id })
 
     definePageMeta({
         layout: 'products'
@@ -14,4 +16,5 @@
 </script>
 
 <style scoped>
+    
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,18 @@
 <template>
-    <p>Products</p>
+    <div class="grid grid-cols-4 gap-5">
+        <div v-for="product in products">
+            <ProductCard :product="product" />
+        </div>
+    </div>
 </template>
 
 <script setup>
     definePageMeta({
         layout: 'products'
     })
+
+    // fetch the products
+    const { data: products } = await useFetch('https://fakestoreapi.com/products')
 </script>
 
 <style scoped>
